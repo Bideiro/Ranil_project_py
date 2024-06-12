68cf0393327c82d7f19b9af345b0f29a15f5192a
bideiro <qd-deocampo@tip.edu.ph>
2024-06-12 11:25:55 +0800

    woring ?

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget
+from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt, pyqtSlot, QFile, QTextStream
 from PyQt5 import QtGui
 from Screens.Login import LoginWindow
 
-# class MainWindow(QWidget):
-#     def __init__(self):
-#         super(MainWindow, self).__init__()
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.MainStack = QtWidgets.QStackedWidget()
+        self.setCentralWidget(self.MainStack)
+        
+        self.Login = LoginWindow()
+        
+        self.MainStack.addWidget(self.Login)
+        self.show()
+
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     
-    a = LoginWindow()
-    
+    Win = MainWindow()
+
     sys.exit(app.exec())
