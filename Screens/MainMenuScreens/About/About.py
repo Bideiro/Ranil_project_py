dec4a2ada4c1193149086ad983dbc2ebc3bb69ac
bideiro <qd-deocampo@tip.edu.ph>
2024-07-03 09:52:02 +0800

    about platform fix

--- Screens/MainMenuScreens/About/About.py
+++ Screens/MainMenuScreens/About/About.py
@@ -1,19 +1,21 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow,QApplication, QPushButton, QWidget
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget
 from PyQt5.QtCore import Qt, pyqtSlot, QFile, QTextStream, QUrl
 from PyQt5.QtGui import QDesktopServices
 
-
-import webbrowser as wb
 from .About_ui import Ui_MainWindow
 from Dialogs.DLog_Alert import DLG_Alert
-
 
 class AboutWindow(QMainWindow, Ui_MainWindow):
 
     def __init__(self):
-        super(AboutWindow,self).__init__()
+        super(AboutWindow, self).__init__()
         self.setupUi(self)
         
-        self.UManual_btn.clicked.connect(lambda: wb.open_new(r'sample-user-manual.pdf'))
-        
+        # Connect the button to the function that opens the PDF
+        self.UManual_btn.clicked.connect(self.open_pdf)
+
+    def open_pdf(self):
+        # Use QDesktopServices to open the PDF file
+        pdf_path = QUrl.fromLocalFile('sample-user-manual.pdf')
+        QDesktopServices.openUrl(pdf_path)
